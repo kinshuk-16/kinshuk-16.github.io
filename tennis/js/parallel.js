99a793da5e39a04bb6ed7aa1ebef9b129a269a16
Kinshuk <kinshuksinha91@yahoo.co.in>
2016-05-05 14:17:56 -0700

    infoviz final

--- tennis/js/parallel.js
+++ tennis/js/parallel.js
@@ -5,54 +5,54 @@ var margin = {top: 30, right: 100, bottom: 20, left: 200},
 
 var dset = "data/parallel2011.csv";
 startD3();
-var yearSelector = function (event) {
-	var element = document.getElementById("year");
-	var selectedValue = element.options[element.selectedIndex].text;
+// var yearSelector = function (event) {
+// 	var element = document.getElementById("year");
+// 	var selectedValue = element.options[element.selectedIndex].text;
     
-    if (selectedValue == "2011") {
-    	dset = "data/parallel2011.csv";
-    	startD3();
-    }
-    else if (selectedValue =="2012") {
-    	dset = "data/parallel2012.csv";
-    	startD3();
-    }
-    else if (selectedValue =="2013") {
-    	dset = "data/parallel2013.csv";
-    	startD3();
-    }
-    else if (selectedValue =="2014") {
-    	dset = "data/parallel2014.csv";
-    	startD3();
-    }
-    else if (selectedValue =="2015") {
-      dset = "data/parallel2015.csv";
-      startD3();
-    }
-    console.log(dset);
-};
+//     if (selectedValue == "2011") {
+//     	dset = "data/parallel2011.csv";
+//     	startD3();
+//     }
+//     else if (selectedValue =="2012") {
+//     	dset = "data/parallel2012.csv";
+//     	startD3();
+//     }
+//     else if (selectedValue =="2013") {
+//     	dset = "data/parallel2013.csv";
+//     	startD3();
+//     }
+//     else if (selectedValue =="2014") {
+//     	dset = "data/parallel2014.csv";
+//     	startD3();
+//     }
+//     else if (selectedValue =="2015") {
+//       dset = "data/parallel2015.csv";
+//       startD3();
+//     }
+//     console.log(dset);
+// };
 
   var sz = document.forms['demoForm'].elements['year'];
 for (var i=0, len=sz.length; i<len; i++) {
     sz[i].onclick = function() { 
         if (this.value == "2011") {
-      dset = "../data/parallel2011.csv";
+      dset = "data/parallel2011.csv";
       startD3();
     }
     else if (this.value =="2012") {
-      dset = "../data/parallel2012.csv";
+      dset = "data/parallel2012.csv";
       startD3();
     }
     else if (this.value =="2013") {
-      dset = "../data/parallel2013.csv";
+      dset = "data/parallel2013.csv";
       startD3();
     }
     else if (this.value =="2014") {
-      dset = "../data/parallel2014.csv";
+      dset = "data/parallel2014.csv";
       startD3();
     }
     else if (this.value =="2015") {
-      dset = "../data/parallel2015.csv";
+      dset = "data/parallel2015.csv";
       startD3();
     }
     console.log(dset);
